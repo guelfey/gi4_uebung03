0fdb9a4dfdd94d51b072608f1f3ca84b4c62cce4
Georg Reinke <guelfey@gmail.com>
2015-05-13 14:36:25 +0200

    use variables for input / output

--- main.asm
+++ main.asm
@@ -1,14 +1,19 @@
+SECTION .data
+	ergebnis DD 0
+	eingabe DD 3
+
 SECTION .text
 	global main
 
 main:
-	mov ebx, 2 ; ebx = x (2)
+	mov ebx, [eingabe] ; ebx = x
 	mov eax, ebx ; eax = x
 	imul eax ; eax = x^2
 	imul eax, 3 ; eax = 3x^2
 	imul ebx, 4 ; ebx = 4x
 	add eax, ebx ; eax = 3x^2 + 4x
 	sub eax, 5 ; eax = 3x^2 + 4x - 5
+	mov [ergebnis], eax
 
 	; end program
 	mov ebx, 0
